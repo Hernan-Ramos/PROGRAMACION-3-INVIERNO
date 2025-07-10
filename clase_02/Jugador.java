99ce19e27f91dbc5ea602d8d5f1d3e7b2b8faf8d
Hernan-Ramos <diegohernanramoschoque@gmail.com>
2025-07-10 10:53:25 -0400

    Add files via upload

---
+++ clase_02/Jugador.java
@@ -0,0 +1,44 @@
+package clase_02;
+
+public class Jugador {
+    public  String nombre;
+    public String carnet;
+    public String sexo;
+    public int edad;
+    public String posicion;
+
+    public Jugador(String nombre,String carnet,String sexo, int edad, String posicion) {
+        this.nombre = nombre;
+        this.carnet = carnet;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombre='" + nombre + '\'' +
+                ", carnet='" + carnet + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", edad=" + edad +
+                ", posicion='" + posicion + '\'' +
+                '}';
+    }
+}
---
+++ clase_02/PilaJugadores.java
@@ -0,0 +1,65 @@
+package clase_02;
+
+public class PilaJugadores {
+    public static void main(String[] args) {
+        Pila p = new Pila(4);
+        Jugador j1 = new Jugador("Diego", "12345", "Masculino", 19, "Delantero");
+        Jugador j2 = new Jugador("jhoana","14683678", "Femenino", 19, "Centro");
+        Jugador j3 = new Jugador("Cris","190467", "Masculino", 18, "Defensa");
+        Jugador j4 = new Jugador("Gus","18934789", "Masculino", 17, "Centro");
+        p.adicionar(j1);
+        p.adicionar(j2);
+        p.adicionar(j3);
+        p.adicionar(j4);
+
+        p.Mostrar();
+        System.out.println("----------------------------");
+        PilaJugadores pj = new PilaJugadores();
+        pj.EliminarporEdad(p);
+        System.out.println("---------------------------");
+        pj.crearPilaMasculino(p);
+    }
+    public void EliminarporEdad(Pila p){
+        Jugador x;
+        Pila paux = new Pila(p.max);
+        boolean sw = false;
+        while(!p.esVacia()){
+            x = p.eliminar();
+
+            if(x.getEdad()<18){
+                sw = true;
+
+            }else{
+                paux.adicionar(x);
+            }
+        }
+        p.Vaciar(paux);
+        if(!sw){
+            System.out.println("No hay ningun Jugador menor de 18");
+        }else{
+            System.out.println("se elimino al siguiente jugador menor de 18 " );
+        }
+        System.out.println("Pila actualizada:");
+        p.Mostrar();
+    }
+
+    public void crearPilaMasculino(Pila p){
+        Pila mas = new Pila(p.max);
+        Pila paux = new Pila(p.max);
+        while(!p.esVacia()){
+            Jugador x = p.eliminar();
+            if(x.getSexo().equals("Masculino")){
+                mas.adicionar(x);
+            }else{
+                paux.adicionar(x);
+
+            }
+
+        }
+        p.Vaciar(paux);
+        System.out.println("pila masculino");
+        mas.Mostrar();
+        System.out.println("pila femenino");
+        p.Mostrar();
+    }
+}
